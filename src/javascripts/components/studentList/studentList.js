1fa52df90eadbe6380cfad380c26a37f9ef36e8a
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-10-05 11:41:27 -0500

    added cards w names pics

---
+++ src/javascripts/components/studentList/studentList.js
@@ -0,0 +1,20 @@
+import util from '../../helpers/util';
+
+import studentData from '../../helpers/data/studentData';
+
+import './studentList.scss';
+
+import studentCard from '../studentCard/studentCard';
+
+
+const createStudentList = () => {
+  const students = studentData.getStudents();
+  let domString = '<ul class="student-list">';
+  for (let i = 0; i < students.length; i += 1) {
+    domString += studentCard.createStudentCard(students[i]);
+  }
+  domString += '<ul>';
+  util.printToDom('student-container', domString);
+};
+
+export default { createStudentList };
